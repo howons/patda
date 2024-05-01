57c49091408e1f399f2d371ab46a0e8829db3f4a
howons <shinhw371@gmail.com>
2024-05-01 22:35:02 +0900

    style: 페이지 & 스토리북 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nanum_Gothic } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const nanumGoth = Nanum_Gothic({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nanumGoth.className}>{children}</body>
     </html>
   );
 }
