c5d56d5bd520055b22c70e644f1e66c00276cf17
howons <shinhw371@gmail.com>
2024-05-07 18:57:58 +0900

    refactor: 이전 플랫폼 기억 기능 훅 분리

--- app/ui/SearchBar/Category.tsx
+++ app/ui/SearchBar/Category.tsx
@@ -1,25 +1,14 @@
 "use client";
 
 import { PLATFORM_NAME, TRANS_DURATION } from "@lib/constants/platform";
+import useLastPlatform from "@lib/hooks/useLastPlatform";
 import { useCategoryStore } from "@lib/providers/CategoryStoreProvider";
-import { usePlatformStore } from "@lib/providers/PlatformStoreProvider";
 import { CategoryDirection, Platform } from "@lib/types/property";
-import {
-  Dispatch,
-  MutableRefObject,
-  SetStateAction,
-  useRef,
-  useState,
-} from "react";
 
 function Category() {
-  const [lastPlatform, setLastPlatform] = useState<Platform>("daangn");
-  const transitionTimer = useRef<NodeJS.Timeout | null>(null);
-  const platform = usePlatformStore((state) => state.platform);
+  const { lastPlatform, platform, isChanging } =
+    useLastPlatform(TRANS_DURATION);
   const { direction, toggleActive } = useCategoryStore((state) => state);
-
-  const shouldChange = platform !== lastPlatform;
-  setTransitionTimer(shouldChange, transitionTimer, setLastPlatform, platform);
 
   const defualtStyle =
     "relative w-24 h-full bg-white rounded-l-full overflow-hidden";
@@ -35,24 +24,24 @@ function Category() {
   };
 
   const lastTransitionStyle: { [key in CategoryDirection]: string } = {
-    up: shouldChange
+    up: isChanging
       ? "top-0 translate-y-full duration-300"
       : "top-full cs:duration-0",
-    left: shouldChange
+    left: isChanging
       ? "left-0 -translate-x-full duration-300"
       : "-left-full cs:duration-0",
-    down: shouldChange
+    down: isChanging
       ? "bottom-0 -translate-y-full duration-300"
       : "bottom-full cs:duration-0",
   };
   const transitionStyle: { [key in CategoryDirection]: string } = {
-    up: shouldChange
+    up: isChanging
       ? "-top-full translate-y-full duration-300"
       : "top-0 cs:duration-0",
-    left: shouldChange
+    left: isChanging
       ? "-left-full translate-x-full duration-300"
       : "left-0 cs:duration-0",
-    down: shouldChange
+    down: isChanging
       ? "-bottom-full -translate-y-full duration-300"
       : "bottom-0 cs:duration-0",
   };
@@ -69,20 +58,6 @@ function Category() {
       </label>
     </div>
   );
-}
-
-function setTransitionTimer(
-  shouldChange: Boolean,
-  transitionTimer: MutableRefObject<NodeJS.Timeout | null>,
-  setLastPlatform: Dispatch<SetStateAction<Platform>>,
-  curPlatform: Platform
-) {
-  if (shouldChange && !transitionTimer.current) {
-    transitionTimer.current = setTimeout(() => {
-      setLastPlatform(curPlatform);
-      transitionTimer.current = null;
-    }, TRANS_DURATION);
-  }
 }
 
 export default Category;
