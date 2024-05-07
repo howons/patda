df64a9ae14de188b3c3ee02fb9eeacc1e3954696
howons <shinhw371@gmail.com>
2024-05-07 21:10:08 +0900

    refactor: 카테고리 목록 순서 조작 로직 훅 분리

--- app/ui/SearchBar/CategorySelector.tsx
+++ app/ui/SearchBar/CategorySelector.tsx
@@ -1,39 +1,15 @@
 "use client";
 
 import { TRANS_DURATION } from "@lib/constants/platform";
+import useCategoryItemList from "@lib/hooks/useCategoryItemList";
 import { useCategoryStore } from "@lib/providers/CategoryStoreProvider";
-import { usePlatformStore } from "@lib/providers/PlatformStoreProvider";
-import { CategoryDirection, Platform } from "@lib/types/property";
 import CategoryItem from "@ui/SearchBar/CategoryItem";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 function CategorySelector() {
-  const [selectedItem, setSelectedItem] = useState(0);
-  const itemList = useRef<Platform[]>(["daangn", "bunjang", "etc", "joongna"]);
   const selectorRef = useRef<HTMLDivElement | null>(null);
-  const { platform, updatePlatform } = usePlatformStore((state) => state);
-  const { updateDirection, isActive, toggleActive } = useCategoryStore(
-    (state) => state
-  );
-
-  selectedItem === 0 && initItemList(platform, itemList.current);
-
-  const handleItemClick = (item: Platform, idx: number) => () => {
-    if (selectedItem !== 0) return;
-
-    setSelectedItem(idx);
-
-    const directions: CategoryDirection[] = ["up", "down", "left", "up"];
-    updateDirection(directions[idx]);
-    updatePlatform(item);
-
-    isActive && toggleActive();
-
-    setTimeout(() => {
-      reorderItemList(idx, itemList.current);
-      setSelectedItem(0);
-    }, TRANS_DURATION);
-  };
+  const isActive = useCategoryStore((state) => state.isActive);
+  const { itemList, selectedItemIdx, handleItemClick } = useCategoryItemList();
 
   useEffect(() => {
     const containerActiveDuration = "cs:duration-300";
@@ -49,11 +25,11 @@ function CategorySelector() {
     }, TRANS_DURATION);
   }, [isActive]);
 
-  const selectorDefaultStyle = `absolute top-1/2 transition-transform ${selectedItem % 2 ? "duration-300" : "duration-0"}`;
+  const selectorDefaultStyle = `absolute top-1/2 transition-transform ${selectedItemIdx % 2 ? "duration-300" : "duration-0"}`;
 
   const selectorStyle = ["rotate-0", "-rotate-90", "rotate-0", "rotate-90"];
 
-  const containerDefaultStyle = `absolute origin-top-left transition-transform ${selectedItem ? "duration-300" : "duration-0"}`;
+  const containerDefaultStyle = `absolute origin-top-left transition-transform ${selectedItemIdx ? "duration-300" : "duration-0"}`;
 
   const containerRotateStyle = [
     "-rotate-45",
@@ -74,7 +50,7 @@ function CategorySelector() {
     [isActive ? "scale-90" : "scale-50", "scale-[.35]", "scale-50", ""],
   ];
 
-  const itemDefaultStyle = `transition-transform ${selectedItem % 2 ? "duration-300" : "duration-0"}`;
+  const itemDefaultStyle = `transition-transform ${selectedItemIdx % 2 ? "duration-300" : "duration-0"}`;
 
   const itemRotateStyle = [
     ["", "rotate-90", "", "-rotate-90"],
@@ -86,41 +62,21 @@ function CategorySelector() {
   return (
     <div
       ref={selectorRef}
-      className={`${selectorDefaultStyle} ${selectorStyle[selectedItem]}`}>
+      className={`${selectorDefaultStyle} ${selectorStyle[selectedItemIdx]}`}>
       {itemList.current.map((item, idx) => (
         <div
           key={item}
-          className={`${containerDefaultStyle} ${containerRotateStyle[idx]} ${containerScaleStyle[idx][selectedItem]}`}
+          className={`${containerDefaultStyle} ${containerRotateStyle[idx]} ${containerScaleStyle[idx][selectedItemIdx]}`}
           onClick={handleItemClick(item, idx)}>
           <CategoryItem
             platform={item}
             isActive={isActive}
-            className={`${itemDefaultStyle} ${idx > 0 ? "cursor-pointer" : ""} ${itemRotateStyle[idx][selectedItem]}`}
+            className={`${itemDefaultStyle} ${idx > 0 ? "cursor-pointer" : ""} ${itemRotateStyle[idx][selectedItemIdx]}`}
           />
         </div>
       ))}
     </div>
   );
-}
-
-function reorderItemList(clickedItem: number, itemList: Platform[]) {
-  switch (clickedItem) {
-    case 1:
-      itemList.push(itemList.shift()!);
-      break;
-    case 2:
-      [itemList[0], itemList[2]] = [itemList[2], itemList[0]];
-      break;
-    case 3:
-      itemList.unshift(itemList.pop()!);
-  }
-}
-
-function initItemList(initItem: Platform, itemList: Platform[]) {
-  if (itemList[0] !== initItem) {
-    const itemIdx = itemList.findIndex((item) => item === initItem);
-    [itemList[0], itemList[itemIdx]] = [itemList[itemIdx], itemList[0]];
-  }
 }
 
 export default CategorySelector;
