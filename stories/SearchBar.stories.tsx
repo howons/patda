d5d8ecd1bc83726ca3102dd53223399b8010e02e
howons <shinhw371@gmail.com>
2024-05-09 22:42:43 +0900

    feat: 카테고리 활성화 인터렉션 추가

--- stories/SearchBar.stories.tsx
+++ stories/SearchBar.stories.tsx
@@ -26,7 +26,7 @@ export const Daangn: Story = {
       name: "검색할 카테고리 이름",
     });
 
-    await step("click other category buttons", async () => {
+    await step("다른 카테고리 버튼을 클릭시 카테고리 변경", async () => {
       await userEvent.click(canvas.getByRole("button", { name: "번개장터" }));
       await waitFor(() => {
         expect(selector.childNodes[0]).toHaveAttribute("name", "번개장터");
@@ -45,6 +45,18 @@ export const Daangn: Story = {
         expect(category.childNodes[1]).toHaveTextContent("기타");
       });
     });
+
+    await step(
+      "현재 카테고리 이름을 클릭시 선택 가능한 카테고리 이름 표시",
+      async () => {
+        await userEvent.click(category);
+        await waitFor(() => {
+          expect(selector.childNodes[1]).toHaveClass("scale-90");
+          expect(selector.childNodes[2]).toHaveClass("scale-75");
+          expect(selector.childNodes[3]).toHaveClass("scale-90");
+        });
+      }
+    );
   },
 };
 
