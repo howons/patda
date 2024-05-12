c3decaaefde2aeaba62393bce219d06cbe7ff929
howons <shinhw371@gmail.com>
2024-05-12 22:39:57 +0900

    feat: 썸네일 컴포넌트 추가

--- app/ui/SearchList/SearchListItem.tsx
+++ app/ui/SearchList/SearchListItem.tsx
@@ -1,13 +1,30 @@
 import { Platform } from "@lib/types/property";
 import { TroublemakerInfo } from "@lib/types/response";
+import Thumbnail from "@ui/Thumbnail";
 
 interface SearchListItemProps {
   itemInfo: TroublemakerInfo;
   platform: Platform;
 }
 
-function SearchListItem({ itemInfo, platform }: SearchListItemProps) {
-  return <li>{itemInfo.nickname}</li>;
+function SearchListItem({
+  itemInfo: { nickname, image },
+  platform,
+}: SearchListItemProps) {
+  const defaultStyle = "w-full flex";
+
+  return (
+    <li className={`${defaultStyle}`}>
+      <Thumbnail
+        platform={platform}
+        src={image}
+        alt="프로필 사진"
+        width={100}
+        height={100}
+      />
+      {nickname}
+    </li>
+  );
 }
 
 export default SearchListItem;
---
+++ app/ui/Thumbnail/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Platform } from "@lib/types/property";
+import { FiMoreHorizontal } from "@react-icons/all-files/fi/FiMoreHorizontal";
+import Image, { ImageProps } from "next/image";
+import { useState } from "react";
+
+import DaangnLogo from "@/public/당근.svg";
+import BunjangLogo from "@/public/번개장터.svg";
+import JoongnaLogo from "@/public/중고나라.svg";
+
+interface ThumbnailProps extends ImageProps {
+  platform: Platform;
+}
+
+function Thumbnail({
+  platform,
+  src,
+  alt,
+  width,
+  height,
+  ...props
+}: ThumbnailProps) {
+  const [isError, setIsError] = useState(false);
+
+  const SubImage =
+    platform === "daangn" ? (
+      <DaangnLogo width={width} height={height} />
+    ) : platform === "bunjang" ? (
+      <BunjangLogo width={width} height={height} />
+    ) : platform === "joongna" ? (
+      <JoongnaLogo width={width} height={height} />
+    ) : (
+      <FiMoreHorizontal width={width} height={height} />
+    );
+
+  return !isError && src ? (
+    <Image
+      src={src}
+      alt={alt ?? "썸네일"}
+      onError={(e) => {
+        setIsError(true);
+      }}
+      {...props}
+    />
+  ) : (
+    SubImage
+  );
+}
+
+export default Thumbnail;
