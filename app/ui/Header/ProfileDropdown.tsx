5860079687386fc5aaa3a06a32bf3e7d4c545763
howons <shinhw371@gmail.com>
2024-06-02 08:02:23 +0900

    style: 로그인 드랍다운 디자인

--- app/ui/Header/ProfileDropdown.tsx
+++ app/ui/Header/ProfileDropdown.tsx
@@ -1,6 +1,14 @@
-import { MenuItem, MenuItems } from "@headlessui/react";
+import {
+  MenuHeading,
+  MenuItem,
+  MenuItems,
+  MenuSection,
+} from "@headlessui/react";
 
 import { providerMap, signIn } from "@/auth";
+import GoogleLogo from "@/public/google.svg";
+import KakaoLogo from "@/public/kakao.svg";
+import NaverLogo from "@/public/naver.svg";
 
 function ProfileDropdown() {
   const providerNameKR: Record<string, string> = {
@@ -9,22 +17,50 @@ function ProfileDropdown() {
     kakao: "카카오",
   };
 
+  const providerColorStyle: Record<string, string> = {
+    google: "text-[#4285F4]",
+    naver: "text-[#03C75A]",
+    kakao: "text-[#FFCD00]",
+  };
+
+  const svgStyle = "size-6 mr-3";
+  const ProviderLogo: Record<string, JSX.Element> = {
+    google: <GoogleLogo className={`fill-[#4285F4] ${svgStyle}`} />,
+    naver: <NaverLogo className={`fill-[#03C75A] ${svgStyle}`} />,
+    kakao: <KakaoLogo className={`fill-yellow-950 ${svgStyle}`} />,
+  };
+
   return (
-    <MenuItems anchor="bottom" className="w-11/12 max-w-96 p-2">
-      {Object.values(providerMap).map(({ id, name }) => (
-        <MenuItem key={id}>
+    <MenuItems
+      anchor={{ to: "bottom", gap: "16px", padding: "12px" }}
+      className="w-96 rounded-l-lg rounded-br-lg bg-white/70 p-3 shadow-lg backdrop-blur">
+      <MenuSection>
+        <MenuHeading className="mb-2 text-sm opacity-50">
+          로그인•회원가입
+        </MenuHeading>
+        {Object.values(providerMap).map(({ id, name }) => (
           <form
             key={id}
             action={async () => {
               "use server";
               await signIn(id);
             }}>
-            <button type="submit">
-              <span>Sign in with {providerNameKR[id] ?? name}</span>
-            </button>
+            <MenuItem>
+              <button
+                type="submit"
+                className="flex h-8 w-full items-center rounded-lg p-6 data-[focus]:bg-gray-400/30">
+                {ProviderLogo[id]}
+                <span>
+                  <b className={`${providerColorStyle[id] ?? ""}`}>
+                    {providerNameKR[id] ?? name}
+                  </b>
+                  로 로그인하기
+                </span>
+              </button>
+            </MenuItem>
           </form>
-        </MenuItem>
-      ))}
+        ))}
+      </MenuSection>
     </MenuItems>
   );
 }
--- app/ui/Header/ProfileMenu.tsx
+++ app/ui/Header/ProfileMenu.tsx
@@ -1,4 +1,4 @@
-import { Menu, MenuButton } from "@headlessui/react";
+import { Menu, MenuButton, Transition } from "@headlessui/react";
 import { IoPersonCircleOutline } from "@react-icons/all-files/io5/IoPersonCircleOutline";
 import ProfileDropdown from "@ui/Header/ProfileDropdown";
 
@@ -8,7 +8,15 @@ function ProfileMenu() {
       <MenuButton className="size-12 rounded-full">
         <IoPersonCircleOutline className="size-full" />
       </MenuButton>
-      <ProfileDropdown />
+      <Transition
+        enter="duration-200 ease-out"
+        enterFrom="scale-95 opacity-0"
+        enterTo="scale-100 opacity-100"
+        leave="duration-300 ease-out"
+        leaveFrom="scale-100 opacity-100"
+        leaveTo="scale-95 opacity-0">
+        <ProfileDropdown />
+      </Transition>
     </Menu>
   );
 }
