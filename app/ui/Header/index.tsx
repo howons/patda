cee29b0239346fb8ed446e7bda0c9ebb13f9297a
howons <shinhw371@gmail.com>
2024-06-04 01:56:08 +0900

    feat: 헤더 로고 추가

--- app/ui/Header/index.tsx
+++ app/ui/Header/index.tsx
@@ -1,10 +1,15 @@
 import HeaderSearch from "@ui/Header/HeaderSearch";
 import ProfileMenu from "@ui/Header/ProfileMenu";
+import Link from "next/link";
+
+import Logo from "@/public/당근빳다.svg";
 
 function Header() {
   return (
     <div className="fixed left-0 top-0 flex h-14 w-screen items-center justify-between border-b border-zinc-300 bg-white px-4">
-      <div>로고</div>
+      <Link href="/">
+        <Logo className="size-12" />
+      </Link>
       <div className="relative h-full grow">
         <HeaderSearch />
       </div>
