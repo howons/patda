04abca1cc5c15e0157e5dbfba2a0593bb2fd468d
howons <shinhw371@gmail.com>
2024-06-04 04:20:10 +0900

    feat: 로고 텍스트 추가

--- app/ui/Header/index.tsx
+++ app/ui/Header/index.tsx
@@ -1,4 +1,5 @@
 import HeaderSearch from "@ui/Header/HeaderSearch";
+import LogoText from "@ui/Header/LogoText";
 import ProfileMenu from "@ui/Header/ProfileMenu";
 import Link from "next/link";
 
@@ -7,8 +8,9 @@ import Logo from "@/public/당근빳다.svg";
 function Header() {
   return (
     <div className="fixed left-0 top-0 flex h-14 w-screen items-center justify-between border-b border-zinc-300 bg-white px-4">
-      <Link href="/">
+      <Link href="/" className="flex">
         <Logo className="size-12" />
+        <LogoText />
       </Link>
       <div className="relative h-full grow">
         <HeaderSearch />
