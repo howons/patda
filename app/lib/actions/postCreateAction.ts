ad365eb6e4e887ce152e3c0828f5e52edaf87a98
howons <shinhw371@gmail.com>
2024-06-17 01:58:28 +0900

    feat: 액션 상태 타입 분리

--- app/lib/actions/postCreateAction.ts
+++ app/lib/actions/postCreateAction.ts
@@ -2,12 +2,13 @@
 
 import { PLATFORM_ID } from "@lib/constants/platform";
 import { TAG_ID } from "@lib/constants/tag";
+import { ActionState } from "@lib/types/action";
 import { z } from "zod";
 
 const formSchema = z.object({
   platform: z.nativeEnum(PLATFORM_ID),
   targetNickname: z.string(),
-  tags: z.nativeEnum(TAG_ID),
+  tag: z.nativeEnum(TAG_ID),
   imageUrls: z.array(z.string()),
   content: z.string(),
   anonymousUserNickname: z.string().nullable(),
@@ -16,22 +17,30 @@ const formSchema = z.object({
 
 export type FormValues = z.infer<typeof formSchema>;
 
-export type State = {
-  status: "success";
-  message: string;
-} | null;
-
 export async function createPost(
-  prevState: State,
+  prevState: ActionState,
   formData: FormData
-): Promise<State> {
+): Promise<ActionState> {
   const input = formSchema.safeParse({
     platform: formData.get("platform"),
     targetNickname: formData.get("targetNickname"),
+    tag: formData.get("tag"),
+    imageUrls: formData.get("imageUrls"),
+    content: formData.get("content"),
+    anonymousUserNickname: formData.get("anonymousUserNickname"),
+    etcPlatformName: formData.get("etcPlatformName"),
   });
 
+  if (!input.success) {
+    const { fieldErrors } = input.error.flatten();
+    return {
+      status: "ERROR_VALIDATE",
+      fieldErrors,
+    };
+  }
+
   return {
-    status: "success",
-    message: "test",
+    status: "SUCCESS",
+    message: "포스트 생성 완료",
   };
 }
---
+++ app/lib/types/action.ts
@@ -0,0 +1,12 @@
+export type ActionState =
+  | {
+      status: "SUCCESS";
+      message: string;
+    }
+  | {
+      status: "ERROR_VALIDATE";
+      fieldErrors: {
+        [field: string]: string[];
+      };
+    }
+  | null;
