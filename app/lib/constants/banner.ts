f2b87f8c20443ab89d6625fe5c64178457f383e6
howons <shinhw371@gmail.com>
2024-07-18 22:58:41 +0900

    refactor: 배너 공통 컴포넌트 변경

--- app/lib/constants/banner.ts
+++ app/lib/constants/banner.ts
@@ -1,1 +1,5 @@
-export const BANNER_IMAGES = ["/당근.svg", "/번개장터.svg", "/중고나라.svg"];
+export const BANNER_IMAGES = [
+  { id: "daangn", url: "/당근.svg" },
+  { id: "bunjang", url: "/번개장터.svg" },
+  { id: "joongna", url: "/중고나라.svg" },
+];
