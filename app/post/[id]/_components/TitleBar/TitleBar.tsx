e7298be94100938f8467d7c95c162871d1f5a95d
howons <shinhw371@gmail.com>
2024-07-18 15:51:00 +0900

    feat: 제목 선 색 추가

--- app/post/[id]/_components/TitleBar/TitleBar.tsx
+++ app/post/[id]/_components/TitleBar/TitleBar.tsx
@@ -20,11 +20,25 @@ export default function TitleBar({
   tag,
   etcPlatformName,
 }: TitlebarProps) {
-  const platformStyle: { [key in Platform]: string } = {
+  const platformNameStyle: { [key in Platform]: string } = {
     daangn: "text-orange-700",
     bunjang: "text-red-700",
     joongna: "text-green-700",
     etc: "text-zinc-700",
+  };
+
+  const platformNicknameStyle: { [key in Platform]: string } = {
+    daangn: "text-orange-800",
+    bunjang: "text-red-800",
+    joongna: "text-green-800",
+    etc: "text-zinc-800",
+  };
+
+  const platformLineStyle: { [key in Platform]: string } = {
+    daangn: "border-orange-300",
+    bunjang: "border-red-300",
+    joongna: "border-green-300",
+    etc: "border-zinc-300",
   };
 
   return (
@@ -41,14 +55,20 @@ export default function TitleBar({
       <div className="flex grow flex-col">
         <div className="ml-24 flex h-14 items-center">
           <div>
-            <Title className={`${platformStyle[platform]}`}>
+            <Title className={`${platformNameStyle[platform]}`}>
               {platform === "etc" ? etcPlatformName : PLATFORM_NAME[platform]}
             </Title>
-            <Title className="2xs:hidden">{targetNickname}</Title>
+            <Title className={`2xs:hidden ${platformNicknameStyle[platform]}`}>
+              {targetNickname}
+            </Title>
           </div>
           <Dot platform={platform} className="mx-5 max-2xs:hidden" />
-          <Title className="max-2xs:hidden">{targetNickname}</Title>
-          <div className="relative ml-8 h-0 grow border-b border-orange-300">
+          <Title
+            className={`max-2xs:hidden ${platformNicknameStyle[platform]}`}>
+            {targetNickname}
+          </Title>
+          <div
+            className={`relative ml-8 h-0 grow border-b ${platformLineStyle[platform]}`}>
             <Dot platform={platform} className="absolute -left-1 -top-1" />
             <Dot
               platform={platform}
