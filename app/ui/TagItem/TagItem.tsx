be71a0da7dbd4ae10854ed8bdfe7b7707fb93ae8
howons <shinhw371@gmail.com>
2024-07-23 18:03:25 +0900

    fix: 아이콘 오지정 수정

--- app/ui/TagItem/TagItem.tsx
+++ app/ui/TagItem/TagItem.tsx
@@ -4,7 +4,7 @@ import { FiMoreHorizontal } from "@react-icons/all-files/fi/FiMoreHorizontal";
 import { GiBrokenPottery } from "@react-icons/all-files/gi/GiBrokenPottery";
 import { GiUmbrella } from "@react-icons/all-files/gi/GiUmbrella";
 import { MdMoneyOff } from "@react-icons/all-files/md/MdMoneyOff";
-import { RiSafariLine } from "@react-icons/all-files/ri/RiSafariLine";
+import { RiSafe2Line } from "@react-icons/all-files/ri/RiSafe2Line";
 import { RiZzzLine } from "@react-icons/all-files/ri/RiZzzLine";
 import type { ComponentProps } from "react";
 
@@ -14,7 +14,7 @@ import type { PostInfo } from "#lib/types/response.js";
 const iconStyle = "size-3/4 rotate-45 fill-neutral-600";
 
 const TagIcons: { [key in TagId]: React.JSX.Element } = {
-  abuse: <RiSafariLine className={iconStyle} />,
+  abuse: <RiSafe2Line className={iconStyle} />,
   cancel: <FaRunning className={iconStyle} />,
   attempt: <FaTheaterMasks className={iconStyle} />,
   noShow: <RiZzzLine className={iconStyle} />,
