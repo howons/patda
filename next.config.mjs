17352ec372696e694dfaed41d6e018c5fcdcd987
howon <shinhw371@naver.com>
2024-08-25 05:16:44 +0900

    fix: next 설정 오류 수정

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: `${process.env.NEXT_PUBLIC_SUPABASE_URL.slice(8)}`,
+        hostname: `${process.env.NEXT_PUBLIC_SUPABASE_HOST}`,
         port: "",
         pathname: "/storage/v1/object/public/patda-images/**",
       },
