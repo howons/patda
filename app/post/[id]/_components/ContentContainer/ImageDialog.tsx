00b29cb6c2a55b804caddf4f59c39fbb4a25d7b8
Howon Shin <shinhw371@gmail.com>
2024-09-16 01:16:22 +0900

    feat: 캐러셀 카운트 추가

--- app/post/[id]/_components/ContentContainer/ImageDialog.tsx
+++ app/post/[id]/_components/ContentContainer/ImageDialog.tsx
@@ -15,6 +15,7 @@ import {
   CarouselContent,
   CarouselItem,
 } from "#components/ui/carousel.jsx";
+import useCarouselCount from "#lib/hooks/useCarouselCount.js";
 import usePopstate from "#lib/hooks/usePopstate.js";
 import type { PostInfo } from "#lib/types/response.js";
 
@@ -36,15 +37,14 @@ export default function ImageDialog({
     condition: isOpen,
   });
 
+  const { setApi, current, count } = useCarouselCount();
+
   return (
     <Dialog open={isOpen} onClose={handleCloseClick} className="relative z-50">
       <DialogBackdrop className="fixed inset-0 bg-black/70" />
       <div className="fixed inset-0 flex h-screen w-screen items-center justify-center p-4">
-        <CloseButton className="absolute right-4 top-4 z-10 cursor-pointer opacity-80 hover:opacity-50">
-          <IoClose className="size-8 fill-neutral-200" />
-        </CloseButton>
         <DialogPanel>
-          <Carousel>
+          <Carousel setApi={setApi}>
             <CarouselContent>
               {images.map((name) => (
                 <CarouselItem key={name} className="h-screen w-screen">
@@ -60,6 +60,12 @@ export default function ImageDialog({
             </CarouselContent>
           </Carousel>
         </DialogPanel>
+        <div className="absolute bottom-4 right-4 text-neutral-200/80">
+          {current} / {count}
+        </div>
+        <CloseButton className="absolute right-4 top-4 cursor-pointer opacity-80 hover:opacity-50">
+          <IoClose className="size-8 fill-neutral-200" />
+        </CloseButton>
       </div>
     </Dialog>
   );
