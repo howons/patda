606ebe10e8abdf8d4332aefdfd50eac72a2254f4
Howon Shin <shinhw371@gmail.com>
2024-11-27 17:30:17 +0900

    feat: 임시저장 키 문법 수정

--- app/lib/hooks/useTempSave.ts
+++ app/lib/hooks/useTempSave.ts
@@ -1,7 +1,5 @@
 "use client";
 
-import { useId, useState } from "react";
-
 import { getStorageItemList, setStorageItem } from "#lib/utils/localStorage.js";
 
 const PREFIX = "patda";
@@ -11,26 +9,23 @@ interface UseTempSaveProps {
 }
 
 export default function useTempSave({ containerId }: UseTempSaveProps) {
-  const [tempId, setTempId] = useState<string>();
-  const initTempId = useId();
-
-  if (tempId === undefined) {
-    setTempId(PREFIX + initTempId);
-  }
+  const storageKey = PREFIX + containerId;
 
   const saveData = (data: { [key: string]: string }) => {
-    if (!tempId) return;
-
-    const dataWithContainerId = { containerId, ...data };
-    return setStorageItem(tempId, JSON.stringify(dataWithContainerId));
+    return setStorageItem(storageKey, JSON.stringify(data));
   };
 
   const getMyDataList = () => {
-    return getStorageItemList(PREFIX).filter(({ data }) => {
+    return getStorageItemList(storageKey).filter(({ data }) => {
       if (!data) return false;
 
       const parsedData = JSON.parse(data);
-      return parsedData?.containerId === containerId;
+      return parsedData;
     });
+  };
+
+  return {
+    saveData,
+    getMyDataList,
   };
 }
